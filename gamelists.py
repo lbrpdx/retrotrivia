e3238c736dfbde20d7ee402828ec57d89586d4de
lbrpdx <lionel.bringuier@gmail.com>
2021-12-30 21:58:35 -0800

    New systems for Batocera 33 + potential crash fix

--- gamelists.py
+++ gamelists.py
@@ -9,9 +9,9 @@ import os
 
 BASEPATH  = '/userdata/roms/'
 XML       = '/gamelist.xml'
-MIN_GAMES = 10   # Don't index if fewer than this
-MAX_GAMES = 5000 # Don't index full sets
-SYSTEMS   = [  '3do', '3ds', 'amiga1200', 'amiga500', 'amigacd32', 'amigacdtv', 'amstradcpc', 'apple2', 'atari2600', 'atari5200', 'atari7800', 'atari800', 'atarist', 'atomiswave', 'c128', 'c20', 'c64', 'channelf', 'colecovision', 'daphne', 'dos', 'dreamcast', 'easyrpg', 'fbneo', 'fds', 'flash', 'fmtowns', 'fpinball', 'gameandwatch', 'gamecube', 'gamegear', 'gb', 'gba', 'gbc', 'gx4000', 'intellivision', 'jaguar', 'lutro', 'lynx', 'mame', 'mastersystem', 'megadrive', 'model3', 'msx', 'msx1', 'msx2', 'msx2+', 'msxturbor', 'mugen', 'n64', 'naomi', 'nds', 'neogeo', 'neogeocd', 'nes', 'ngp', 'ngpc', 'o2em', 'openbor', 'pc88', 'pc98', 'pcengine', 'pcenginecd', 'pcfx', 'pico8', 'pokemini', 'ports', 'prboom', 'ps2', 'ps3', 'psp', 'psx', 'satellaview', 'saturn', 'scummvm', 'sega32x', 'segacd', 'sg1000', 'snes', 'solarus', 'sufami', 'supervision', 'supergrafx', 'thomson', 'tic80', 'vectrex', 'virtualboy', 'wii', 'wiiu', 'windows', 'wswan', 'wswanc', 'x1', 'xbox', 'x68000', 'zx81', 'zxspectrum' ]
+MIN_GAMES = 10    # Don't index if fewer than this
+MAX_GAMES = 10000 # Don't index full sets
+SYSTEMS   = [ '3do', '3ds', 'abuse', 'adam', 'advision', 'amiga1200', 'amiga500', 'amigacd32', 'amigacdtv', 'amstradcpc', 'apfm1000', 'apple2', 'apple2gs', 'arcadia', 'atari2600', 'atari5200', 'atari7800', 'atari800', 'atarist', 'atomiswave', 'bbc', 'c128', 'c20', 'c64', 'cavestory', 'cdi', 'channelf', 'coco', 'colecovision', 'cplus4', 'daphne', 'dos', 'dreamcast', 'easyrpg', 'ecwolf', 'fbneo', 'fds', 'flash', 'fm7', 'fmtowns', 'fpinball', 'gamate', 'gameandwatch', 'gamecom', 'gamecube', 'gamegear', 'gb', 'gba', 'gbc', 'gmaster', 'gx4000', 'intellivision', 'j2me', 'jaguar', 'lcdgames', 'lutro', 'lynx', 'mame', 'mastersystem', 'megadrive', 'megaduck', 'model2', 'model3', 'msx', 'msx1', 'msx2', 'msx2+', 'msxturbor', 'mugen', 'n64', 'n64dd', 'naomi', 'nds', 'neogeo', 'neogeocd', 'nes', 'ngp', 'ngpc', 'o2em', 'openbor', 'opentyrian', 'pc88', 'pc98', 'pcengine', 'pcenginecd', 'pcfx', 'pet', 'pico8', 'pokemini', 'ports', 'prboom', 'ps2', 'ps3', 'psp', 'psx', 'pv1000', 'samcoupe', 'satellaview', 'saturn', 'scummvm', 'scv', 'sega32x', 'segacd', 'sg1000', 'sgb', 'snes', 'nes-msu1', 'solarus', 'sufami', 'supervision', 'supergrafx', 'thomson', 'ti99', 'tic80', 'tutor', 'tvgames', 'uzebox', 'vectrex', 'virtualboy', 'wii', 'wiiu', 'windows', 'wswan', 'wswanc', 'x1', 'xbox', 'x68000', 'zc210', 'zx81', 'zxspectrum' ]
 
 class gamelist:
     def __init__(self):
@@ -29,9 +29,13 @@ class gamelist:
             return q
         root = self.tree.getroot()
         setgames = set()
-        for item in root.findall('game/name'):
-            short=item.text.split('(')[0] # Remove (USA, Europe...)
-            setgames.add(short.rstrip(' '))
+        try:
+            for item in root.findall('game/name'):
+                short=item.text.split('(')[0] # Remove (USA, Europe...)
+                setgames.add(short.rstrip(' '))
+        except Exception as e:
+            print("No game with video for {}".format(system))
+            return q
         allgames = [n for n in setgames]
         if len(allgames) < MIN_GAMES:
             return q
